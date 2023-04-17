626fcf711ae53cebd38070171df42d43af2bcdfe
Jack Kamire <jacmire@gmail.com>
2023-04-17 05:14:58 +0300

    app routing changes

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+# App views
+def say_hello(request):
+    return HttpResponse("Hey Jack!")
